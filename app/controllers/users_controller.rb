963355759e1877ce351a9c39612612da58024964
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-11-26 17:26:02 +0300

    complete assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+    def show 
+        user =User.find(params[:id])
+        render json: user, except:[:created_at, :updated_at], include: :items
+    end
+
 end
